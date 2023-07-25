54eb90639a98dbc80df95f373516b84bc44b5c21
Hiren Patel <hp2715136@gmail.com>
2023-07-25 15:07:23 +0530

    wip

--- src/feature/api/apiSlince.js
+++ src/feature/api/apiSlince.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'apiSlice',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://03f0-103-204-189-64.ngrok-free.app/api/',
+        baseUrl: 'https://85e9-103-204-189-64.ngrok-free.app/api/',
     }),
     // tagTypes: ['Post'],
     endpoints: (builder) => {
